0204070711c699adedb9372cc0ef7819905da1a7
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-14 22:38:52 -0400

    a

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Test } from './src/components/Test';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
     <>
-      <Test />
-      <Text>Olá Mundo</Text>
+      <Dashboard />
     </>
   );
 }
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
+const styles = StyleSheet.create({
+  container: {},
+});
