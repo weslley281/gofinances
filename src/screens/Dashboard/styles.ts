eed231a76f7d14ca65e5be41093923123f5aeaf8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-19 20:13:51 -0400

    finally

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
-import { FlatList } from 'react-native';
+import { FlatList, FlatListProps } from 'react-native';
 import {
   getBottomSpace,
   getStatusBarHeight,
@@ -90,7 +90,9 @@ export const Title = styled.Text`
 `;
 
 export const TransactionList = styled(
-  FlatList as new () => FlatList<DataListProps>
+  FlatList as new (
+    props: FlatListProps<DataListProps>
+  ) => FlatList<DataListProps>
 ).attrs({
   showsVerticalScrollIndicator: false,
   contentContainerStyle: {
