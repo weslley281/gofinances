e335db16700568239fab7457b4a502fc1ede8eee
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-24 13:07:59 -0400

    a

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,13 +5,57 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { useTheme } from 'styled-components';
+import { Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
