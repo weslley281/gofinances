40fcb7001b8ea14a9571bfe31d40d604dbaa535c
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-30 07:41:00 -0400

    a

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 import { useTheme } from 'styled-components';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -49,7 +50,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -22,19 +22,3 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
 `;
-
-export const Form = styled.View`
-  flex: 1;
-  width: 100%;
-  padding: 24px;
-  justify-content: space-between;
-`;
-
-export const Fields = styled.View``;
-
-export const TransactionsTypes = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  margin-top: 8px;
-  margin-bottom: 16px;
-`;
