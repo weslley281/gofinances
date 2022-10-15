aedc0c97cca2417e8bd319ce3a171175fe5f76b4
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-15 07:44:31 -0400

    a

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -7,3 +7,21 @@ test('Checks if the user input with place holder is on the screen', () => {
   const imputName = getByPlaceholderText('Nome');
   expect(imputName.props.placeholder).toBeTruthy();
 });
+
+test('Checks if the user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const imputName = getByTestId('input-name');
+  const imputSurName = getByTestId('input-surname');
+
+  expect(imputName.props.value).toEqual('Weslley');
+  expect(imputSurName.props.value).toEqual('Ferraz');
+});
+
+test('Checks if title render correct', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const title = getByTestId('title');
+
+  expect(title.props.children).toContain('Perfil');
+});
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,9 +4,20 @@ import { Button, Text, TextInput, View } from 'react-native';
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
-      <TextInput placeholder="Nome" autoCorrect={false} />
-      <TextInput placeholder="Sobrenome" />
+      <Text testID="title">Perfil</Text>
+      <TextInput
+        testID="input-name"
+        placeholder="Nome"
+        autoCorrect={false}
+        value="Weslley"
+      />
+
+      <TextInput
+        testID="input-surname"
+        placeholder="Sobrenome"
+        value="Ferraz"
+      />
+
       <Button title="Salvar" onPress={() => {}} />
     </View>
   );
