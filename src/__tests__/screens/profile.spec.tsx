89416e3b6b8fa73b33a593839223756213dea340
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-15 13:51:55 -0400

    a

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { Profile } from '../../screens/Profile';
 
-test('Checks if the user input with place holder is on the screen', () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const imputName = getByPlaceholderText('Nome');
-  expect(imputName.props.placeholder).toBeTruthy();
-});
+describe('Profile', () => {
+  it('Checks if the user input with place holder is on the screen', () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const imputName = getByPlaceholderText('Nome');
+    expect(imputName.props.placeholder).toBeTruthy();
+  });
 
-test('Checks if the user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />);
+  it('Checks if the user data has been loaded', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const imputName = getByTestId('input-name');
-  const imputSurName = getByTestId('input-surname');
+    const imputName = getByTestId('input-name');
+    const imputSurName = getByTestId('input-surname');
 
-  expect(imputName.props.value).toEqual('Weslley');
-  expect(imputSurName.props.value).toEqual('Ferraz');
-});
+    expect(imputName.props.value).toEqual('Weslley');
+    expect(imputSurName.props.value).toEqual('Ferraz');
+  });
 
-test('Checks if title render correct', () => {
-  const { getByTestId } = render(<Profile />);
+  it('Checks if title render correct', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const title = getByTestId('title');
+    const title = getByTestId('title');
 
-  expect(title.props.children).toContain('Perfil');
+    expect(title.props.children).toContain('Perfil');
+  });
 });
